d48fac5c58fea4b227d4d1acaf0761d9e4fb81d1
mikhaylovskya <an.mikhayloskaya@gridnine.com>
2025-03-14 17:58:55 +0100

    feat: add help command

---
+++ src/cli/commands/help.command.ts
@@ -0,0 +1,24 @@
+import { CommandName } from "./command.constant.js";
+import { Command } from "./command.interface.js";
+
+const HELP_MESSAGE = `
+    Программа для подготовки данных для REST API сервера.  
+
+    Пример: cli.js --<command> [--arguments]
+    Команды:
+
+    ${CommandName.Version}:     # выводит номер версии
+    ${CommandName.Help}:        # печатает этот текст
+    ${CommandName.Import}:      # импортирует данные из TSV
+    ${CommandName.Generate}:    # генерирует произвольное количество тестовых данных
+`;
+
+export class HelpCommand implements Command {
+    public getName(): string {
+        return CommandName.Help;
+    }
+
+    public async execute(..._parameters: string[]): Promise<void> {
+        console.info(HELP_MESSAGE);
+    }
+}
