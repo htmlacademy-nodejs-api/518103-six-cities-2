24a0a8156a1ff1241773609b78a92ea9725fa4cc
mikhaylovskya <an.mikhayloskaya@gridnine.com>
2025-03-24 12:43:33 +0100

    feat: add cli application

---
+++ src/cli/cli-application.ts
@@ -0,0 +1,17 @@
+import { Command } from "./commands/command.interface.js";
+
+type CommandCollection = Record<string, Command>;
+
+export class CLIApplication {
+    private readonly commandCollection: CommandCollection = {};
+    
+    public registerCommands(commandList: Command[]): void {
+        commandList.forEach((command) => {
+            if (Object.hasOwn(this.commandCollection, command.getName())) {
+                throw new Error(`Command ${command.getName()} is already registered.`);
+            }
+
+            this.commandCollection[command.getName()] = command;
+        });
+    }
+}
