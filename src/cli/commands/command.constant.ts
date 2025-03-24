f0f774111d6a85ce74ec6f98a43a029ced43cdd4
mikhaylovskya <an.mikhayloskaya@gridnine.com>
2025-03-24 12:51:25 +0100

    feat: add command parser

--- src/cli/commands/command.constant.ts
+++ src/cli/commands/command.constant.ts
@@ -1,6 +1,8 @@
+export const COMMAND_PREFIX = '--';
+
 export const enum CommandName {
-    Version = '--version',
-    Help = '--help',
-    Import = '--import',
-    Generate = '--generate',
+    Version = `${COMMAND_PREFIX}version`,
+    Help = `${COMMAND_PREFIX}help`,
+    Import = `${COMMAND_PREFIX}import`,
+    Generate = `${COMMAND_PREFIX}generate`,
   }
